c8fd2eec3da86cfc16da1ed22257197e56d89159
Nitesh Singh <niteshsingh.ns44@gmail.com>
2022-07-26 14:14:19 +0530

    JobControllers Updated

--- Controllers/JobController.cs
+++ Controllers/JobController.cs
@@ -22,9 +22,53 @@ namespace jobPortal.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<List<Job>>> postJobs(Job jobs)
+        public async Task<ActionResult<List<Job>>> postJobs(List<Job> jobs)
         {
-            _context.job.Add(jobs);
+            _context.job.AddRange(jobs);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.job.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Job>> getJobById(int id)
+        {
+            var findJob = await _context.job.SingleOrDefaultAsync(job => job.jobId == id);
+
+            if (findJob == null)
+            {
+                return BadRequest(string.Format("Job Not Found"));
+            }
+            return Ok(findJob);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult<List<Job>>> deleteJobById(int id)
+        {
+            var findJob = await _context.job.SingleOrDefaultAsync(job => job.jobId == id);
+            {
+                if (findJob == null)
+                {
+                    return BadRequest(string.Format("Job not Found"));
+                }
+                _context.job.Remove(findJob);
+                await _context.SaveChangesAsync();
+                return Ok(await _context.job.ToListAsync());
+            }
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<Job>>> updateJobById(Job requestJob)
+        {
+            var findJob = await _context.job.FindAsync(requestJob.jobId);
+            if (findJob == null)
+            {
+                return BadRequest(string.Format("Job not Found"));
+            }
+            findJob.postDate = requestJob.postDate;
+            findJob.jobTitle = requestJob.jobTitle;
+            findJob.jobLocation = requestJob.jobLocation;
+            findJob.jobDescription = requestJob.jobDescription;
             await _context.SaveChangesAsync();
             return Ok(await _context.job.ToListAsync());
         }
