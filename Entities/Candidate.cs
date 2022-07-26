02f42b7588ac8dcaf05b251df5bce1ba4f316bce
Nitesh Singh <niteshsingh.ns44@gmail.com>
2022-07-26 10:52:51 +0530

    Candidate entity added

---
+++ Entities/Candidate.cs
@@ -0,0 +1,20 @@
+namespace jobPortal.Entities{
+    public record Candidate{
+
+        public int CandidateId{get;set;}
+
+        public string CandidateName{get; set;}
+
+        public string CandidateContact{ get; set;}
+
+        public string CandidateEmail{ get; set;}
+
+        public string CandidateResume{ get; set;}
+
+        public string CandidateQualification{ get; set;}
+
+        
+
+
+    }
+}
